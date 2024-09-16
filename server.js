6d46e71996de9681e7bc6849f756bc81212f12d2
gscar2018 <gscardo@ymail.com>
2024-09-15 21:53:12 -0400

    Added server, Axios, and ChartJS
    Added server, Axios, and ChartJS

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const { title } = require('process');
+const app = express();
+const port = 3000;
+
+app.use('/', express.static('public'));
+
+const budget = {
+    myBudget: [
+        {
+            title: 'Eat Out',
+            budget: 30
+        }, {
+            title: 'Rent',
+            budget: 375
+        }, {
+            title: 'Grocery',
+            budget: 90
+        }
+    ]
+};
+
+
+app.get('/hello', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.get('/budget', (req, res) => {
+    res.json(budget);
+});
+
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
